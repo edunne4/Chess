e7605399d08dfe3b13f7ff088f6b520626bed3a5
Ethan Dunne <ebd007@bucknell.edu>
2019-11-10 12:11:24 -0500

    Created Board and Squares classes

---
+++ src/model/Square.java
@@ -0,0 +1,24 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: Ethan Dunne
+ * Section: 11am
+ * Date: 11/10/19
+ * Time: 11:58 AM
+ *
+ * Project: csci205finalproject
+ * Package: model
+ * Class: Square
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package model;
+
+public class Square {
+
+    ChessPiece currentPiece;
+}
