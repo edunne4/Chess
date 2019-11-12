7e27e2f9ac3ffa4541ee615948f425c965c130ce
Ryan Bailis <rb066@bucknell.edu>
2019-11-12 10:50:51 -0500

    added comments

--- src/Import3DMaker/JavaFX3DsImporter.java
+++ src/Import3DMaker/JavaFX3DsImporter.java
@@ -1,11 +1,11 @@
-//Code taken from http://www.interactivemesh.org/models/jfx3dimporter.html
+//A portion of this code was taken from http://www.interactivemesh.org/models/jfx3dimporter.html
+//The camera motion aspects of this code were taken from https://stackoverflow.com/questions/28731460/javafx-moving-3d-objects-with-mouse-on-a-virtual-plane
 
 package Import3DMaker;
 
 import com.interactivemesh.jfx.importer.ImportException;
 import com.interactivemesh.jfx.importer.stl.StlMeshImporter;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Point3D;
 import javafx.geometry.Pos;
@@ -13,7 +13,6 @@ import javafx.scene.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.PickResult;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.*;
@@ -22,19 +21,10 @@ import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
-/**
- * @author $Author: tmartinet$
- * @version $FullVersion$
- * @mavengroupid $GroupId$
- * @mavenartifactid $ArtifactId$
- */
 public class JavaFX3DsImporter extends Application {
 
     private PerspectiveCamera camera;
-    private final double sceneWidth = 800;
-    private final double sceneHeight = 600;
 
     private double mousePosX;
     private double mousePosY;
@@ -53,59 +43,69 @@ public class JavaFX3DsImporter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+        //initialize the root
         root = new HBox();
         root.setSpacing(300);
-
-        MeshView piece1 = createChessPiece("king.stl",Color.RED);
-        MeshView piece2 = createChessPiece("king.stl",Color.BLUE);
-
-
-        root.getChildren().addAll(piece1,piece2);
         root.setPadding(new Insets(20));
         root.setAlignment(Pos.CENTER);
 
+        //create three pieces
+        MeshView piece1 = createChessPiece("king.stl",Color.RED);
+        MeshView piece2 = createChessPiece("king.stl",Color.BLUE);
+        MeshView piece3 = createChessPiece("Knight.stl",Color.GREEN);
 
-        scene = new Scene(root, 1024, 800);
+        //move piece1 to create some 3D perspective
+        piece1.setTranslateX(100);
+        piece1.setTranslateY(100);
+
+        //add the three pieces to the root
+        root.getChildren().addAll(piece1,piece2,piece3);
+
+        //initialize the camera
         camera = new PerspectiveCamera(true);
         camera.setVerticalFieldOfView(false);
-
         camera.setNearClip(0.1);
         camera.setFarClip(100000.0);
         camera.getTransforms().addAll (rotateX, rotateY, new Translate(0, 0, -3000));
 
-//        PointLight light = new PointLight(Color.GAINSBORO);
-//        root.getChildren().add(light);
-//        root.getChildren().add(new AmbientLight(Color.WHITE));
-
+        //initialize the scene, and set the camera to the scene
+        scene = new Scene(root, 1024, 800);
         scene.setCamera(camera);
 
-        eventHandlers();
+        //initialize all of the event handlers to deal with moving the camera
+        moveCameraEvents();
 
+        //show the scene to the user
         stage.setScene(scene);
         stage.show();
 
     }
 
+    /**
+     * A function to create a MeshView object from an STL file.
+     * @param filename the STL file to be parsed and converted to a JavaFX MesView object
+     * @param color the color of the MeshView in the viewport
+     * @return a MeshView object that will be viewable in the scene
+     */
     private MeshView createChessPiece(String filename, Color color) {
-        StlMeshImporter stlImporter = new StlMeshImporter();
 
+        //create a StlMeshImporter object and try parsing with the filename
+        StlMeshImporter stlImporter = new StlMeshImporter();
         try {
-            //stlImporter.read(this.getClass().getResource("Support_Sphere_V2.stl"));
             stlImporter.read(this.getClass().getResource(filename));
         }
         catch (ImportException e) {
-            System.out.println("Error!");
+            System.err.println("Error. STL file does not exist.");
             e.printStackTrace();
-            return null;
         }
 
-
+        //create a MeshView object from the StlMeshImporter
         stlImporter.getImport();
-        System.out.println(stlImporter.getImport());
         TriangleMesh mesh = stlImporter.getImport();
         stlImporter.close();
         MeshView meshView = new MeshView(mesh);
 
+        //set the material, general properties, and scale of the mesh
         meshView.setMaterial(new PhongMaterial(color));
         meshView.setDrawMode(DrawMode.FILL);
         meshView.setVisible(true);
@@ -113,23 +113,28 @@ public class JavaFX3DsImporter extends Application {
         meshView.setScaleY(10);
         meshView.setScaleZ(10);
 
+        //rotate the mesh
         meshView.setRotationAxis(Rotate.X_AXIS);
         meshView.setRotate(90);
+
         return meshView;
     }
 
-    public void eventHandlers() {
-//        scene.setOnMousePressed((MouseEvent me) -> {
-//            mousePosX = me.getSceneX();
-//            mousePosY = me.getSceneY();
-//            PickResult pr = me.getPickResult();
-//            if(pr!=null && pr.getIntersectedNode() != null && pr.getIntersectedNode() instanceof Sphere){
-//                distance=pr.getIntersectedDistance();
-//                s = (Sphere) pr.getIntersectedNode();
-//                isPicking=true;
-//                vecIni = unProjectDirection(mousePosX, mousePosY, scene.getWidth(),scene.getHeight());
-//            }
-//        });
+    /**
+     * A method containing a series of event handlers to move the camera around the scene
+     */
+    public void moveCameraEvents() {
+        scene.setOnMousePressed((MouseEvent me) -> {
+            mousePosX = me.getSceneX();
+            mousePosY = me.getSceneY();
+            PickResult pr = me.getPickResult();
+            if(pr!=null && pr.getIntersectedNode() != null && pr.getIntersectedNode() instanceof Sphere){
+                distance=pr.getIntersectedDistance();
+                s = (Sphere) pr.getIntersectedNode();
+                isPicking=true;
+                vecIni = unProjectDirection(mousePosX, mousePosY, scene.getWidth(),scene.getHeight());
+            }
+        });
         scene.setOnMouseDragged((MouseEvent me) -> {
             mousePosX = me.getSceneX();
             mousePosY = me.getSceneY();
@@ -178,7 +183,6 @@ public class JavaFX3DsImporter extends Application {
         Point3D res = localToScene(dir);
         return res.subtract(localToScene(new Point3D(0, 0, 0)));
     }
-
 
     public static void main(String[] args) {
         launch(args);
