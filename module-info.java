80f81bcfaa6b0fd6fa42afa91165e2d551f0740d
Ryan Bailis <rb066@bucknell.edu>
2019-11-14 23:02:31 -0500

    Revert "starting separate view classes"
    This reverts commit 6662eea2

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module csci205.labs {
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.base;
-    requires java.desktop;
-    opens src.BoardView;
-
-}
