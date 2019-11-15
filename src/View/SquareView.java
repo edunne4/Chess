19ad683be06d981ebc62cf8aa618728a565f59cb
Jake Schaeffer <jas128@bucknell.edu>
2019-11-15 11:41:31 -0500

    refactored board and square view classes to inherit from Pane classes

--- src/View/SquareView.java
+++ src/View/SquareView.java
@@ -18,30 +18,53 @@
  */
 package View;
 
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
-public class SquareView {
+public class SquareView extends StackPane {
 
     private StackPane pane;
     private int row;
     private int col;
 
 
-    public SquareView(int row,int col){
-        pane = new StackPane();
+    public SquareView(int row, int col) {
+        super(); //this
         this.row = row;
         this.col = col;
     }
 
-    public void addPieceView(PieceView pv){
-        pane.getChildren().add(pv.getView());
+    public void addPieceView(PieceView pv) {
+        this.getChildren().add(pv.getView());
     }
 
-    public void setSize(int size){
-        pane.setPrefSize(size,size);
+    public void setSize(int size) {
+        this.setPrefSize(size, size);
     }
 
-    public StackPane getPane(){return pane;}
+    public void addImageView(ImageView iv) {
+        this.getChildren().add(iv);
+    }
+
+    public void highlight() {
+
+    this.setStyle("-fx-background-color: #7dff9d");
+
+    }
+
+    public void unHighlight(){
+        String squareColor;
+        if ((row + col) % 2 == 0) {
+            squareColor = BoardView.LIGHT_COLOR_HEX;
+        } else {
+            squareColor = BoardView.DARK_COLOR_HEX;
+        }
+        this.setStyle("-fx-background-color: " + squareColor);
+    }
+
+    public ImageView getPiece(){
+        return (ImageView)this.getChildren().get(0);
+    }
 
     public int getRow() {
         return row;
