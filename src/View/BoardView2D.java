d5da92872bf9ad0b545d48f22b3f9e87191fd508
Ethan Dunne <ebd007@bucknell.edu>
2019-12-02 12:14:13 -0500

    Merged gah daammmnn

--- src/View/BoardView2D.java
+++ src/View/BoardView2D.java
@@ -25,73 +25,96 @@ import javafx.geometry.Pos;
  */
 public class BoardView2D extends BoardView {
 
-    //private TilePane board;
-
     static final String LIGHT_COLOR_HEX = "#ede8da";
     static final String DARK_COLOR_HEX = "#949085";
     static final String LIGHT_HIGHLIGHT_HEX = "#b2edb3";
     static final String DARK_HIGHLIGHT_HEX = "#7bbd7b";
 
 
-    BoardView2D(int size){
+    /**
+     * creates a board that is a tile pane and puts a SquareView object in each spot
+     * @param size pixel width and height of the board
+     */
+    public BoardView2D(int size){
         //the board will be a grid of squares
         super();
         this.setPrefSize(size,size);
         //for every one of the 64 squares
         for (int row = 0; row < SIDE_LENGTH; row++) {
             for (int col = 0; col < SIDE_LENGTH; col++) {
-                //make it a stack pane. it will have a color background and the piece can be placed on top of it
-                SquareView2D square = new SquareView2D(row,col);
-                square.setSize(size/SIDE_LENGTH);
-
-
-                square.setAlignment(Pos.CENTER); // make sure piece will be put in center
-
-
-                //pick color of square
-                String squareColor;
-                if ((row + col) % 2 != 0) {
-                    squareColor = LIGHT_COLOR_HEX;
-                } else {
-                    squareColor = DARK_COLOR_HEX;
-                }
-                //set square color
-                square.setStyle("-fx-background-color: " + squareColor);
-                //add square to the board
-                this.getChildren().add(square);
-
+                initSquare(size, row, col);
             }
         }
+
         initPieces();
     }
 
+    /**Creates a SquareView at the specified spot on the board
+     *
+     * @param size the size of the board
+     * @param row the row on the board
+     * @param col the column on the board
+     */
+    private void initSquare(int size, int row, int col) {
+        //make each square a SquareView
+        SquareView2D square = new SquareView2D(row,col);
+        //pick color of square
+        String squareColor;
+        if ((row + col) % 2 != 0) {
+            squareColor = LIGHT_COLOR_HEX;
+        } else {
+            squareColor = DARK_COLOR_HEX;
+        }
+        //set square color
+        square.setStyle("-fx-background-color: " + squareColor);
+        this.getChildren().add(square);
+    }
+
+    /**
+     * initializes the board with all white and black chess pieces
+     */
     void initBoard(){
+        initWhitePieces();
+        initBlackPieces();
+    }
+
+    /**
+     * initializes black pieces in correct spots on the board
+     */
+    private void initBlackPieces() {
         //placeholder to avoid errors
         PieceView piece = new PieceView("https://via.placeholder.com/60");
         //top side
         for (int i = 0; i < SIDE_LENGTH*2; i++) {
-            //white rooks
+            //black rooks
             if(i == 0 || i == 7) {piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png");}
             //queen
             if(i == 4){piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png");}
             //king
             if(i == 3){piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png");}
-            //white bishops
+            //black bishops
             if(i == 2 || i == 5) {piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png");}
             //knights
             if(i == 1 || i == 6) {piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png");}
-            //white pawns
+            //black pawns
             if(i >=8) {piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png");}
 
             SquareView2D square = (SquareView2D)this.getChildren().get(i);
             square.addImageView(piece.getView());
         }
+    }
+
+    /**
+     * initializes white pieces in correct spots on the board
+     */
+    private void initWhitePieces() {
+        PieceView piece = new PieceView("https://via.placeholder.com/60");
         //bottom side
         for (int i = 0; i < SIDE_LENGTH*2; i++) {
 
-            //black pawns
+            //white pawns
             if(i < 8) {piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png");}
-            //black rooks
+            //white rooks
             if(i == 8 || i ==15){piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png");}
             //queen
             if(i == 12){piece = new PieceView("https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png");}
@@ -105,9 +128,16 @@ public class BoardView2D extends BoardView {
             SquareView2D square = (SquareView2D)this.getChildren().get(i + offset);
             square.addImageView(piece.getView());
         }
-
     }
 
+    /**
+     * Moves a piece's image from one squareView to a new squareview on the baord
+     *
+     * @param oldRow the row of the old square
+     * @param oldCol the column of the old square
+     * @param newRow the row of the new square
+     * @param newCol the column of the new square
+     */
     public void movePiece(int oldRow,int oldCol,int newRow,int newCol){
         SquareView2D oldLocationSquare = this.getSquareAt(oldRow,oldCol);
         SquareView2D newLocationSquare = this.getSquareAt(newRow,newCol);
@@ -122,6 +152,13 @@ public class BoardView2D extends BoardView {
     }
 
 
+    /**
+     * Grabs the SquareView at the specified spot in the board
+     *
+     * @param row the square's row
+     * @param col the square's column
+     * @return the SquareView
+     */
     public SquareView2D getSquareAt(int row, int col) {
         return (SquareView2D)this.getChildren().get((7-row) * SIDE_LENGTH + col);
     }
--- src/View/SquareView2D.java
+++ src/View/SquareView2D.java
@@ -25,19 +25,36 @@ public class SquareView2D extends SquareView {
 
     //private StackPane pane;
 
-
+    /**Creates a StackPane object for a square that stores the square's location on the board
+     *
+     * @param row the square's row
+     * @param col the square's column
+     */
     SquareView2D(int row, int col) {
         super(7-row, col); //this
 //        this.row = 7-row;
 //        this.col = col;
     }
 
+
+    /**
+     * sets size based off a pixel size
+     * @param size the pixel size of the square
+     */
     void setSize(int size) {
         this.setPrefSize(size, size);
     }
 
+    /**
+     * Adds a child to this square view
+     * @param iv the ImageView node to be added
+     */
     void addImageView(ImageView iv) { this.getChildren().add(iv); }
 
+
+    /**
+     * Highlights the selected square
+     */
     public void highlight() {
         String squareColor;
         if ((row + col) % 2 == 0) {
@@ -48,6 +65,10 @@ public class SquareView2D extends SquareView {
         this.setStyle("-fx-background-color: " + squareColor);
     }
 
+
+    /**
+     * unHighlights the selected square
+     */
     public void unHighlight(){
         String squareColor;
         if ((row + col) % 2 == 0) {
@@ -58,16 +79,12 @@ public class SquareView2D extends SquareView {
         this.setStyle("-fx-background-color: " + squareColor);
     }
 
+    /**
+     * grabs the imageview contained within the square
+     * @return the ImageView node of the piece
+     */
     public ImageView getPiece(){
         return (ImageView)this.getChildren().get(0);
     }
-
-//    public int getRow() {
-//        return row;
-//    }
-//
-//    public int getCol() {
-//        return col;
-//    }
 
 }
