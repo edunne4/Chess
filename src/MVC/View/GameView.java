ff5dced77ce3fc3661c4ff808e42d929874d183c
Jake Schaeffer <jas128@bucknell.edu>
2019-12-08 19:47:36 -0500

    fixed

--- src/MVC/View/GameView.java
+++ src/MVC/View/GameView.java
@@ -84,8 +84,6 @@ public class GameView {
         VBox boardCoordContainer = new VBox();
         sideCoordAndBoardContainer = new HBox();
 
-        reloadGameView();
-
         boardCoordContainer.getChildren().add(sideCoordAndBoardContainer);
         //put this whole shabang in the root, an HBox
         gameHBox.getChildren().add(boardCoordContainer);
@@ -100,6 +98,7 @@ public class GameView {
         gameMenuBar = new GameMenuBar();
         root = new VBox();
         root.getChildren().addAll(gameMenuBar,gameHBox);
+        reloadGameView();
 
     }
 
